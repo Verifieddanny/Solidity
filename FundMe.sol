14af3ca484f1e13abfcdf75c6452ee1f1f1841ef
Verifieddanny <dannyclassic56@gmail.com>
2023-08-01 20:16:17 +0100

    Solidity push

--- FundMe.sol
+++ FundMe.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.18;
-
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 contract FundMe{
 
     uint256 public  minimumUsd = 5;
@@ -10,10 +10,22 @@ contract FundMe{
      require(msg.value >= minimumUsd, "didn't send enough ETH");
  }
 
- function getPrice() public {
+ function getPrice() public view returns (uint256) {
      //Address (0x694AA1769357215DE4FAC081bf1f309aDC325306)
      // and ABI
+    AggregatorV3Interface pricefeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+    (, int256 price,,,) = pricefeed.latestRoundData();
+     return uint256(price * 1e10);
 
  }
- function getConversionRate() public {}
+ function getConversionRate(uint256 ethAmount) public view returns(uint256) {
+     uint256 ethPrice = getPrice();
+     uint256 ethAmountInUsd = (ethPrice * ethAmount)/ 1e18;
+
+     return  ethAmountInUsd;
+ }
+
+ function getVersion() public  view  returns (uint256) {
+     return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+ }
 }
